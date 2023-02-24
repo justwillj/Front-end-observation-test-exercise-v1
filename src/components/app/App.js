a876e0c5bb27c1155003d730941928639a3aaaca
Justin Williams <williams.jman@gmail.com>
2023-02-24 15:10:11 -0500

    Removed lines of code that was not being used

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Button from "../button/Button.js";
-import { clear } from "@testing-library/user-event/dist/clear";
 import Modal from "../modal/Modal";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [style, setStyle] = useState("");
   const evenArray = new Set();
   const evenArray2 = [];
   const oddArray = new Set();
   const oddArray2 = [];
-  const evenArraySet = new Set();
-  const [isActive, setIsActive] = useState(false);
   /**generates an array of even numbers consecutivly 1-25 */
   const makeAnEvenArray = () => {
     for (let i = 0; i < 25; i++) {
@@ -68,43 +64,8 @@ function App() {
       }, 1000);
   }, []);
 
-  const [even, setEven] = useState(
-    // Array.from(evenArray)
-    evenArray2
-    //   [
-
-    //   { id: 1, number: 2 },
-    //   { id: 2, number: 4 },
-    //   { id: 3, number: 6 },
-    //   { id: 4, number: 8 },
-    //   { id: 5, number: 10 },
-    //   { id: 6, number: 12 },
-    //   { id: 7, number: 14 },
-    //   { id: 8, number: 16 },
-    //   { id: 9, number: 18 },
-    //   { id: 10, number: 20 },
-    //   { id: 11, number: 22 },
-    //   { id: 12, number: 24 },
-    // ]
-  );
-  const [odd, setOdd] = useState(
-    oddArray2
-    //   [
-    //   { id: 1, number: 1 },
-    //   { id: 2, number: 3 },
-    //   { id: 3, number: 5 },
-    //   { id: 4, number: 7 },
-    //   { id: 5, number: 9 },
-    //   { id: 6, number: 11 },
-    //   { id: 7, number: 13 },
-    //   { id: 8, number: 15 },
-    //   { id: 9, number: 17 },
-    //   { id: 10, number: 19 },
-    //   { id: 11, number: 21 },
-    //   { id: 12, number: 23 },
-    //   { id: 13, number: 25 },
-    // ]
-  );
+  const [even, setEven] = useState(evenArray2);
+  const [odd, setOdd] = useState(oddArray2);
 
   const shuffleButton = (list, setList) => {
     const shuffle = list.sort(() => Math.random() - 0.5);
@@ -116,12 +77,11 @@ function App() {
   useEffect(() => {
     makeAnEvenArray();
     makeAnOddArray();
-    console.log(evenArray2);
     shuffleButton(even, setEven);
     shuffleButton(odd, setOdd);
   }, []);
 
-  const buttonCheck = (id, number, list, setList, click, setClicks) => {
+  const buttonCheck = (number, list, setList, click, setClicks) => {
     if (count == 24) {
       setFinishedTime(gameTime);
       setWinMessage(true);
@@ -161,35 +121,28 @@ function App() {
       <div className="main-table">
         <div className="even-grid">
           <h1>Even clicks: {evenClicks}</h1>
-          {even.map(({ id, number, isActive }) => (
+          {even.map(({ id, number }) => (
             <Button
               key={id}
               type="button"
               value={number}
               className={number <= count ? "active" : "test"}
               onClick={() =>
-                buttonCheck(
-                  id,
-                  number,
-                  even,
-                  setEven,
-                  evenClicks,
-                  setEvenClicks
-                )
+                buttonCheck(number, even, setEven, evenClicks, setEvenClicks)
               }
             />
           ))}
         </div>
         <div className="odd-grid">
           <h1>Odd clicks: {oddClicks}</h1>
-          {odd.map(({ id, number, isActive }) => (
+          {odd.map(({ id, number }) => (
             <Button
               key={id}
               type="button"
               value={number}
               className={number <= count ? "active" : "test"}
               onClick={() =>
-                buttonCheck(id, number, odd, setOdd, oddClicks, setOddClicks)
+                buttonCheck(number, odd, setOdd, oddClicks, setOddClicks)
               }
             />
           ))}
@@ -198,5 +151,4 @@ function App() {
     </div>
   );
 }
-// buttonOnClick={shuffleButton}
 export default App;
